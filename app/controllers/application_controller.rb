2787cca54fd73882519fa3a983bb2e970f951034
francosta <francisco@fcosta.pt>
2019-04-25 12:06:31 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/index' do
+    erb :index
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(params)
+    erb :index
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+  end
+
 end
